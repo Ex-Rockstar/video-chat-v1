8fdad8972e34dc71ee98a394b9f1fff80abb874e
Ruthika <ruthirockstar2218@gmail.com>
2023-12-30 12:21:52 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+let localStream;
+let remoteStream;
+let init = async () =>{
+    localStream =await navigator.mediaDevices.getUserMedia({video:true,audio:false})
+    document.getElementById('user-1').srcObject = localStream
+
+}
+
+
+init()
